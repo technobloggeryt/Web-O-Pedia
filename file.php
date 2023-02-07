26be1948e4d5d62bdf376081ba97dd5b5e15c9e7
technobloggeryt <124683880+technobloggeryt@users.noreply.github.com>
2023-02-07 21:21:54 +0530

    Create file.php

---
+++ file.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Useful Websites</title>
+  </head>
+  <body>
+    <h1>Useful Websites</h1>
+    <form action="">
+      <input type="text" placeholder="Search for a website">
+      <button type="submit">Search</button>
+    </form>
+    <ul>
+      <li><a href="https://www.google.com">Google</a></li>
+      <li><a href="https://www.youtube.com">YouTube</a></li>
+      <li><a href="https://www.facebook.com">Facebook</a></li>
+      <li><a href="https://www.amazon.com">Amazon</a></li>
+      <li><a href="https://www.wikipedia.org">Wikipedia</a></li>
+    </ul>
+  </body>
+</html>
